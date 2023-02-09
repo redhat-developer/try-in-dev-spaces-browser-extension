9b2ab555f3f269d31fda1a72b81d631037fbd7e8
David Kwon <dakwon@redhat.com>
2023-02-09 17:59:53 -0500

    Add license header
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- src/gitServices/github/GitHubService.ts
+++ src/gitServices/github/GitHubService.ts
@@ -1,3 +1,8 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
 import { Endpoint, getEndpoints } from "../../preferences";
 import { GitService } from "../GitService";
 import { getFactoryUrl, getHostName, getProjectURL } from "../util";
