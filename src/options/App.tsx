cd1e70f45f07d073c24fe6400cc45c99d4f41256
David Kwon <dakwon@redhat.com>
2023-03-02 09:08:01 -0500

    Implement endpoint inputbox validation with Patternfly
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -22,6 +22,7 @@ import {
     TextInput,
     CardTitle,
 } from "@patternfly/react-core";
+import { ExclamationCircleIcon } from "@patternfly/react-icons";
 import {
     Endpoint,
     getEndpoints,
@@ -32,6 +33,9 @@ import { EndpointsList } from "./EndpointsList";
 export const App = () => {
     const [endpoints, setEndpoints] = useState<Endpoint[]>([]);
     const [newEndpointUrl, setNewEndpointUrl] = useState<string>("");
+    
+    type validate = "success" | "error" | "default";
+    const [newEndpointStatus, setNewEndpointStatus] = useState<validate>("default");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -41,16 +45,46 @@ export const App = () => {
         fetchData().catch(console.error);
     }, []);
 
+    const handleNewEndpointUrlChange = (newUrl: string, _event: React.FormEvent<HTMLInputElement>) => {
+        setNewEndpointUrl(newUrl);
+        if (newUrl === "") {
+          setNewEndpointStatus("default");
+        } else if (isUrl(newUrl)) {
+          setNewEndpointStatus("success");
+        } else {
+          setNewEndpointStatus("error");
+        }
+    };
+
+    const isUrl = (str: string) => {
+        try {
+            new URL(str);
+        } catch {
+            return false;
+        }
+        return true;
+    }
+
     const addNewEndpoint = async () => {
+        const sanitizedEndpoint = sanitizeEndpoint(newEndpointUrl);
         const newEndpoints = endpoints.concat({
-            url: newEndpointUrl,
+            url: sanitizedEndpoint,
             active: false,
             readonly: false,
         });
         await saveEndpoints(newEndpoints);
         await setEndpoints(newEndpoints);
         setNewEndpointUrl("");
+        setNewEndpointStatus("default");
     };
+
+    const sanitizeEndpoint = (str: string) => {
+        let res = str;
+        while (res.charAt(res.length - 1) === "/") {
+            res = res.substring(0, res.length - 1);
+        }
+        return res;
+    }
 
     const setDefault = async (endpoint: Endpoint) => {
         const newEndpoints = [...endpoints];
@@ -84,20 +118,24 @@ export const App = () => {
                 <Split>
                     <SplitItem className="form-text-input">
                         <Form>
-                            <FormGroup>
+                            <FormGroup
+                                validated={newEndpointStatus}
+                                helperTextInvalid="The new endpoint must be a URL"
+                                helperTextInvalidIcon={<ExclamationCircleIcon />}>
                                 <TextInput
                                     type="text"
                                     aria-label="new endpoint"
+                                    validated={newEndpointStatus}
                                     value={newEndpointUrl}
                                     placeholder="Add endpoint"
-                                    onChange={setNewEndpointUrl}
+                                    onChange={handleNewEndpointUrlChange}
                                 />
                             </FormGroup>
                         </Form>
                     </SplitItem>
                     <SplitItem className="form-fill" isFilled></SplitItem>
                     <SplitItem>
-                        <Button variant="primary" onClick={addNewEndpoint} isDisabled={newEndpointUrl.length === 0}>
+                        <Button variant="primary" onClick={addNewEndpoint} isDisabled={newEndpointUrl.length === 0 || newEndpointStatus === "error"}>
                             Add
                         </Button>
                     </SplitItem>
