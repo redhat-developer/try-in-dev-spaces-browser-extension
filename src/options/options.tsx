8db82d1e173587715476278e021f8e7cc5d9840c
David Kwon <dakwon@redhat.com>
2023-03-02 14:09:53 -0500

    Edit styles per browser
    Chromium browsers except Edge can have the options page be sized by
    CSS. Increasing the width of the options page for these browsers
    provides a better viewing experience.
    
    If this is done for Firefox and Edge, there are scrollbars making it
    difficult to use the options page.
    
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -3,10 +3,19 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+
+applyStyles();
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+function applyStyles() {
+    const isChromium = navigator.userAgent.includes("Chrome")
+    const isEdge = isChromium && navigator.userAgent.includes("Edg");
+    if (isChromium && !isEdge) {
+        document.body.classList.add("chromium-body");
+    }
+}
