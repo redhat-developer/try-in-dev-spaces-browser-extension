34e1785474134d8d26ec0b62fd3b55476a3efafc
David Kwon <dakwon@redhat.com>
2023-03-29 11:03:53 -0400

    Don't depend on button class name when finding 'Code' button
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- src/contentScript/buttonInjector/github/GitHubButtonInjector.tsx
+++ src/contentScript/buttonInjector/github/GitHubButtonInjector.tsx
@@ -35,7 +35,7 @@ export class GitHubButtonInjector implements ButtonInjector {
     }
 
     private static codeBtnExists(element: Element): boolean {
-        const btnList = element.getElementsByClassName("Button--primary Button");
+        const btnList = element.getElementsByTagName("summary");
         for (const btn of btnList) {
             if ((btn as HTMLElement).innerText.indexOf("Code") > -1) {
                 return true;
